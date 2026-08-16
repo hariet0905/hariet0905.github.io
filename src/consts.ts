bc039cf24be5aaed68cde4fb2d13abe9404c4138
ha vu <havu@MacBook-Pro-cua-ha-2.local>
2026-08-16 17:29:53 +0700

    Update SITE_COVER to behind.jpg

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@ export const SITE_DESCRIPTION = "CTF writeups by Halwk";
 export const SITE_AUTHOR = "halwk";
 export const SITE_URL = "https://hariet0905.github.io";
 export const SITE_AVATAR = "/avt.jpg";
-export const SITE_COVER = "/behind.jfif";
+export const SITE_COVER = "/behind.jpg";
 
 export const PAGE_SIZE = 10;
 
