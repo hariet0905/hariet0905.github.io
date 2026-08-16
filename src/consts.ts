2dd389c156145d7ca97af44b0cdedebf7209e140
ha vu <havu@MacBook-Pro-cua-ha-2.local>
2026-08-16 16:52:00 +0700

    Update site info

--- src/consts.ts
+++ src/consts.ts
@@ -1,19 +1,17 @@
-export const SITE_TITLE = 'HieuPenguinnn Blog';
-export const SITE_DESCRIPTION = 'CVE & CTF writeups by HieuPenguinnn';
-export const SITE_AUTHOR = 'HieuPenguinnn';
-export const SITE_URL = 'https://santisify.top';
-export const SITE_AVATAR = '/avt.jpg';
-export const SITE_COVER = '/behind.jfif';
+export const SITE_TITLE = "Halwk Blog";
+export const SITE_DESCRIPTION = "CTF writeups by Halwk";
+export const SITE_AUTHOR = "halwk";
+export const SITE_URL = "https://hariet0905.github.io";
+export const SITE_AVATAR = "/avt.jpg";
+export const SITE_COVER = "/behind.jfif";
 
 export const PAGE_SIZE = 10;
 
 export const NAV_ITEMS = [
-  { label: 'Home', href: '/' },
-  { label: 'About', href: '/about' },
+  { label: "Home", href: "/" },
+  { label: "About", href: "/about" },
 ];
 
 export const SOCIAL_LINKS = [
-  { name: 'Telegram', href: 'https://t.me/hieupenguinnn', icon: 'telegram' },
-  { name: 'GitHub', href: 'https://github.com/HieuPenguinnn', icon: 'github' },
-  { name: 'Email', href: 'https://mail.google.com/mail/?view=cm&fs=1&to=ngdinhhieu207@gmail.com', icon: 'mail' },
+  { name: "GitHub", href: "https://github.com/hariet0905", icon: "github" },
 ];
